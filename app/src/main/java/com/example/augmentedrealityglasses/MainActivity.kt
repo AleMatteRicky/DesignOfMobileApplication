23af30a99382daa197a5dff0dc5f8335aa3d11cd
Alessio Ginolfi <alessio.ginolfi@mail.polimi.it>
2025-04-13 21:51:44 +0200

    feat: add checkRecordAudioPermission method

--- app/src/main/java/com/example/augmentedrealityglasses/MainActivity.kt
+++ app/src/main/java/com/example/augmentedrealityglasses/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.augmentedrealityglasses
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat.requestPermissions
+import androidx.core.content.ContextCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -17,12 +21,26 @@ class MainActivity : ComponentActivity() {
             val navController = rememberNavController()
             NavHost(navController = navController, startDestination = ScreenName.HOME.name) {
                 composable(ScreenName.HOME.name) {
-                    Greeting(
-                        name = "Android"
-                    )
+                    checkRecordAudioPermission()
                 }
             }
         }
+    }
+
+    @Composable
+    fun checkRecordAudioPermission(){
+        if(audioPermissionGranted()){
+            //program regularly shows translation feature screen
+        }
+        else{
+            requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), 10) //todo replace 10 with a constant
+            //graceful degrade the translation feature
+            //override onRequestPermissionsResult with code related to this permission
+        }
+    }
+
+    private fun audioPermissionGranted(): Boolean{
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED
     }
 }
 
