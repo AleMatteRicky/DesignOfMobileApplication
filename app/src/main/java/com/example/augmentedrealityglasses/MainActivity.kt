fb3d5a14c7b5b0e404bf7fbddbd85e895045866f
Alessio Ginolfi <alessio.ginolfi@mail.polimi.it>
2025-04-24 19:35:59 +0200

    feat: add first draft of TranslationScreen and related methods

--- app/src/main/java/com/example/augmentedrealityglasses/MainActivity.kt
+++ app/src/main/java/com/example/augmentedrealityglasses/MainActivity.kt
@@ -1,53 +1,128 @@
 package com.example.augmentedrealityglasses
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.core.app.ActivityCompat.requestPermissions
 import androidx.core.content.ContextCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.augmentedrealityglasses.translation.TranslationViewModel
+import com.google.mlkit.nl.translate.TranslateLanguage
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
             NavHost(navController = navController, startDestination = ScreenName.HOME.name) {
                 composable(ScreenName.HOME.name) {
-                    checkRecordAudioPermission()
+                    HomeScreen(
+                        onNavigateToTranslation = {
+                            navController.navigate(
+                                route = ScreenName.TRANSLATION.name
+                            )
+                        },
+                        audioPermissionGranted = audioPermissionGranted()
+                    )
+                }
+
+                composable(ScreenName.TRANSLATION.name) {
+                    CheckRecordAudioPermission() //todo check if it works when permission are refused 1 time
+                    TranslationScreen(
+                        onNavigateToHome = {
+                            navController.navigate(
+                                route = ScreenName.HOME.name
+                            )
+                        },
+                        TranslationViewModel(systemLanguage = TranslateLanguage.ITALIAN, application)
+                    ) //todo update with system language from settings
                 }
             }
         }
     }
 
     @Composable
-    fun checkRecordAudioPermission(){
-        if(audioPermissionGranted()){
-            //program regularly shows translation feature screen
-        }
-        else{
-            requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), 10) //todo replace 10 with a constant
+    fun CheckRecordAudioPermission() {
+        if (!audioPermissionGranted()) {
+            requestPermissions(
+                this,
+                arrayOf(Manifest.permission.RECORD_AUDIO),
+                10
+            ) //todo replace 10 with a constant
             //graceful degrade the translation feature
             //override onRequestPermissionsResult with code related to this permission
         }
     }
 
-    private fun audioPermissionGranted(): Boolean{
-        return ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED
+    fun audioPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.RECORD_AUDIO
+        ) == PackageManager.PERMISSION_GRANTED
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HomeScreen(onNavigateToTranslation: () -> Unit, audioPermissionGranted: Boolean) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        Button(onClick = {
+            onNavigateToTranslation()
+        }) {
+            Text("Translation Screen")
+        }
+    }
 }
+
+@SuppressLint("MissingPermission")
+@Composable
+fun TranslationScreen(onNavigateToHome: () -> Unit, viewModel: TranslationViewModel) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        var recordingButtonText by remember { mutableStateOf("Record") }
+        Button(onClick = {
+            if (viewModel.uiState.isRecording) {
+                viewModel.stopRecording()
+                recordingButtonText = "Record"
+            } else {
+                viewModel.startRecording()
+                recordingButtonText = "Stop Recording"
+            }
+        }, modifier = Modifier.align(Alignment.Center)) {
+            Text(recordingButtonText)
+        }
+
+        Text(
+            text = viewModel.uiState.recognizedText,
+            modifier = Modifier.align(Alignment.TopStart)
+        )
+        Text(
+            text = viewModel.uiState.translatedText,
+            modifier = Modifier.align(Alignment.BottomStart)
+        )
+    }
+
+}
+
+
