02bdfcaa5aea49d01b7edec84c7a223afcf6f3c5
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2025-04-25 16:04:39 +0200

    feat: add BleManager class to have an interface between the view model and the BleDevice

---
+++ app/src/main/java/com/example/augmentedrealityglasses/ble/device/BleManager.kt
@@ -0,0 +1,56 @@
+package com.example.augmentedrealityglasses.ble.device
+
+import android.content.Context
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.shareIn
+
+/*
+    Proxy between the application and the device.
+    It exposes the information from the BleDevice to change the UI
+*/
+class BleManager (
+    private val scope : CoroutineScope = CoroutineScope(Dispatchers.Default + SupervisorJob())
+)
+{
+    private var _bleDevice : BleDevice? = null
+    private var connectionStatus : Flow<DeviceConnectionState>? = null
+    private val TAG = "BleManager"
+
+    fun setDataSource(bleDevice: BleDevice) {
+        if (_bleDevice != null) {
+            Log.d(TAG, "cleaning previous connection")
+            // a new device has been added, hence close the flow from the previous one
+            close()
+        }
+        this._bleDevice = bleDevice
+    }
+
+    fun connect(context: Context) {
+        if (connectionStatus == null) {
+            connectionStatus = _bleDevice?.connect(context)?.shareIn(
+                scope = scope,
+                replay = 1,
+                started = SharingStarted.Lazily
+            )
+        }
+    }
+
+    fun receiveUpdates() : Flow<DeviceConnectionState> {
+        return connectionStatus as Flow<DeviceConnectionState>
+    }
+
+    fun send(msg : String) {
+        _bleDevice?.send(msg)
+    }
+
+    fun close() {
+        scope.cancel()
+        _bleDevice?.close()
+    }
+}
