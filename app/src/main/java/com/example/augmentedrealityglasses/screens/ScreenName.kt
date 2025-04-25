6e2d8283a00a90e533dbc0ffd94ff4d4bdd26b73
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2025-04-25 11:12:29 +0200

    feat: add screen names

---
+++ app/src/main/java/com/example/augmentedrealityglasses/screens/ScreenName.kt
@@ -0,0 +1,10 @@
+package com.example.augmentedrealityglasses.screens
+
+enum class ScreenName {
+    HOME,
+    FIND_DEVICE,
+    ERROR_SCREEN,
+    CONNECT_SCREEN,
+    WEATHER_SCREEN,
+    TRANSLATION_SCREEN
+}
