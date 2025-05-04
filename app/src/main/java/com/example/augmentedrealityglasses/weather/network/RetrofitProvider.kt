9c282cf2dd64aceab9c5a81c7cfcf949fa6226e9
Matteo Gatti <matteo7.gatti@mail.polimi.it>
2025-05-05 01:02:21 +0200

    refactor: merge Retrofit instance creation directly into RetrofitProvider and delete RetroInstance.kt

--- app/src/main/java/com/example/augmentedrealityglasses/weather/network/RetrofitProvider.kt
+++ app/src/main/java/com/example/augmentedrealityglasses/weather/network/RetrofitProvider.kt
@@ -1,8 +1,15 @@
 package com.example.augmentedrealityglasses.weather.network
 
+import com.example.augmentedrealityglasses.weather.constants.Constants
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 //Singleton object for the retrofit service
 object RetrofitProvider {
     val retroService: RetroService by lazy {
-        RetroInstance.getRetroInstance().create(RetroService::class.java)
+        Retrofit.Builder()
+            .baseUrl(Constants.WEATHER_API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build().create(RetroService::class.java)
     }
 }
