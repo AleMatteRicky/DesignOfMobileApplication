5b006a61fbd24ac0be59b3d29e7b52b40a329c78
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2025-05-09 17:59:49 +0200

    feat: add abstract class to handle notification to not repeat the code multiple times

--- app/src/main/java/com/example/augmentedrealityglasses/notifications/PhoneCallReceiver.kt
+++ app/src/main/java/com/example/augmentedrealityglasses/notifications/PhoneCallReceiver.kt
@@ -1,35 +1,21 @@
 package com.example.augmentedrealityglasses.notifications
 
-import android.app.NotificationManager
 import android.util.Log
-import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.telephony.TelephonyManager
-import com.example.augmentedrealityglasses.App
 import com.example.augmentedrealityglasses.ble.device.RemoteDeviceManager
 
-class PhoneCallReceiver : BroadcastReceiver() {
+class PhoneCallReceiver : NotificationReceiver() {
     private val TAG: String = "PhoneCallReceiver"
 
-    override fun onReceive(context: Context, intent: Intent?) {
+    override fun pushNotification(context: Context, intent: Intent?, bleManager: RemoteDeviceManager) {
         Log.d(TAG, "Calling on receive")
-        val bleManager: RemoteDeviceManager =
-            (context.applicationContext as App).container.bleManager
 
         val incomingCall = "incoming_call"
 
         intent?.let {
             val action: String? = it.action
-
-            // check if DND is active
-            val notificationManager =
-                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            val filter = notificationManager.currentInterruptionFilter
-            if (isDNDActive(filter)) {
-                Log.d(TAG, "DND is active: do not share the notification")
-                return
-            }
 
             if (TelephonyManager.ACTION_PHONE_STATE_CHANGED == action) {
                 val state = intent.getStringExtra(TelephonyManager.EXTRA_STATE)
@@ -43,18 +29,12 @@ class PhoneCallReceiver : BroadcastReceiver() {
                             "Incoming call event not transmitted to the device because they are offline"
                         )
                     }
-                } else{
+                } else {
                     Log.d(TAG, "Not interesting state")
                 }
-            } else{
+            } else {
                 Log.d(TAG, "Broadcast has received an unrecognised action $action")
             }
         }
-    }
-
-    private fun isDNDActive(filter: Int): Boolean {
-        return filter == NotificationManager.INTERRUPTION_FILTER_NONE
-                || filter == NotificationManager.INTERRUPTION_FILTER_ALARMS
-                || filter == NotificationManager.INTERRUPTION_FILTER_PRIORITY
     }
 }
