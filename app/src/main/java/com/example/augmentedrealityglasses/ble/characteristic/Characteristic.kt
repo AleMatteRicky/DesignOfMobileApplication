e8e1305728aedb5b2b988885aa40641bc8ff14d8
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2025-08-02 21:18:57 +0200

    feat: add integer to characteristic property and a function to get them directly from the integer representation

--- app/src/main/java/com/example/augmentedrealityglasses/ble/characteristic/Characteristic.kt
+++ app/src/main/java/com/example/augmentedrealityglasses/ble/characteristic/Characteristic.kt
@@ -1,22 +1,36 @@
 package com.example.augmentedrealityglasses.ble.characteristic
 
+import android.bluetooth.BluetoothGattCharacteristic.PROPERTY_INDICATE
+import android.bluetooth.BluetoothGattCharacteristic.PROPERTY_NOTIFY
+import android.bluetooth.BluetoothGattCharacteristic.PROPERTY_READ
+import android.bluetooth.BluetoothGattCharacteristic.PROPERTY_WRITE
 import java.util.UUID
 
 interface Characteristic {
     /**
      * UUID for the characteristic
      */
-    val uuid : UUID
+    val uuid: UUID
 
     /**
      * Property of this characteristic
      */
-    val properties : List<CharacteristicProperty>
+    val properties: Set<CharacteristicProperty>
 
-    enum class CharacteristicProperty {
-        WRITE,
-        READ,
-        INDICATE, // receive ack back from the peripheral
-        NOTIFY // do not receive an ack back from the peripheral
+    enum class CharacteristicProperty(val intRep : Int) {
+        WRITE(PROPERTY_WRITE),
+        READ(PROPERTY_READ),
+        INDICATE(PROPERTY_INDICATE), // receive ack back from the peripheral
+        NOTIFY (PROPERTY_NOTIFY)// do not receive an ack back from the peripheral
     }
+}
+
+fun Int.toCharacteristicProperties(): Set<Characteristic.CharacteristicProperty> {
+    val properties = mutableSetOf<Characteristic.CharacteristicProperty>()
+    for (property in Characteristic.CharacteristicProperty.entries) {
+        if(this.and(property.intRep) != 0) {
+            properties.add(property)
+        }
+    }
+    return properties
 }
