d59174ae0c45701eb4dfc1f9b0eb5bff59e7182b
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2025-08-04 15:13:40 +0200

    feat: add broader characteristic properties and related functions to get them from the Android integer representation

--- app/src/main/java/com/example/augmentedrealityglasses/ble/characteristic/Characteristic.kt
+++ app/src/main/java/com/example/augmentedrealityglasses/ble/characteristic/Characteristic.kt
@@ -1,9 +1,6 @@
 package com.example.augmentedrealityglasses.ble.characteristic
 
-import android.bluetooth.BluetoothGattCharacteristic.PROPERTY_INDICATE
-import android.bluetooth.BluetoothGattCharacteristic.PROPERTY_NOTIFY
-import android.bluetooth.BluetoothGattCharacteristic.PROPERTY_READ
-import android.bluetooth.BluetoothGattCharacteristic.PROPERTY_WRITE
+import android.bluetooth.BluetoothGattCharacteristic
 import java.util.UUID
 
 interface Characteristic {
@@ -17,20 +14,51 @@ interface Characteristic {
      */
     val properties: Set<CharacteristicProperty>
 
-    enum class CharacteristicProperty(val intRep : Int) {
-        WRITE(PROPERTY_WRITE),
-        READ(PROPERTY_READ),
-        INDICATE(PROPERTY_INDICATE), // receive ack back from the peripheral
-        NOTIFY (PROPERTY_NOTIFY)// do not receive an ack back from the peripheral
+    enum class CharacteristicProperty {
+        WRITE, // can be written
+        SIGNED_WRITE, // encrypted => pairing if written
+        WRITE_WITHOUT_RESPONSE, // write, no need for ack back
+        READ, // can be actively read (pull notification)
+        INDICATE, // do not send an ack back to the peripheral (push notification)
+        NOTIFY, // do send an ack back from the peripheral (push notification)
+    }
+
+    companion object {
+        fun isWritable(props: Set<CharacteristicProperty>): Boolean {
+            return props.contains(CharacteristicProperty.WRITE)
+                    || props.contains(CharacteristicProperty.SIGNED_WRITE)
+                    || props.contains(CharacteristicProperty.WRITE_WITHOUT_RESPONSE)
+        }
+
+        fun isReadable(props: Set<CharacteristicProperty>): Boolean {
+            return props.contains(CharacteristicProperty.READ)
+                    || props.contains(CharacteristicProperty.INDICATE)
+                    || props.contains(CharacteristicProperty.NOTIFY)
+        }
     }
 }
 
 fun Int.toCharacteristicProperties(): Set<Characteristic.CharacteristicProperty> {
     val properties = mutableSetOf<Characteristic.CharacteristicProperty>()
-    for (property in Characteristic.CharacteristicProperty.entries) {
-        if(this.and(property.intRep) != 0) {
-            properties.add(property)
-        }
+
+    if (this and BluetoothGattCharacteristic.PROPERTY_INDICATE != 0) {
+        properties.add(Characteristic.CharacteristicProperty.INDICATE)
     }
+    if (this and BluetoothGattCharacteristic.PROPERTY_NOTIFY != 0) {
+        properties.add(Characteristic.CharacteristicProperty.NOTIFY)
+    }
+    if (this and BluetoothGattCharacteristic.PROPERTY_READ != 0) {
+        properties.add(Characteristic.CharacteristicProperty.READ)
+    }
+    if (this and BluetoothGattCharacteristic.PROPERTY_SIGNED_WRITE != 0) {
+        properties.add(Characteristic.CharacteristicProperty.SIGNED_WRITE)
+    }
+    if (this and BluetoothGattCharacteristic.PROPERTY_WRITE != 0) {
+        properties.add(Characteristic.CharacteristicProperty.WRITE)
+    }
+    if (this and BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE != 0) {
+        properties.add(Characteristic.CharacteristicProperty.WRITE_WITHOUT_RESPONSE)
+    }
+
     return properties
 }
