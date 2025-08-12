d9c9b006b8a1a9182547de9e272a750ebb5c1b7a
1Alessio6 <alessio.ginolfi@mail.polimi.it>
2025-08-12 11:54:51 +0200

    feat: improved BottomNavigationBar animation

--- app/src/main/java/com/example/augmentedrealityglasses/MainActivity.kt
+++ app/src/main/java/com/example/augmentedrealityglasses/MainActivity.kt
@@ -9,6 +9,10 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -21,8 +25,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat.requestPermissions
 import androidx.core.content.ContextCompat
@@ -54,12 +60,23 @@ class MainActivity : ComponentActivity() {
             val navigationBarHeight = screenHeight * 0.111f //could be a global variable
             val navigationBarVisible = remember { mutableStateOf(true) }
 
+
             Scaffold(
                 bottomBar = {
                     AnimatedVisibility(
+
+                        //todo check if with other px slideVertically works without glitch
+                        //Slide moves the component but it does not change its dimension so in order
+                        //to have the other elements that follow the vertical slide we need shrink and expand
+
+
                         visible = navigationBarVisible.value,
-                        enter = slideInVertically(),
-                        exit = slideOutVertically()
+                        enter = slideInVertically { 0 } + expandVertically(
+                            expandFrom = Alignment.Top
+                        ) + fadeIn(
+                            initialAlpha = 0.3f
+                        ),
+                        exit = slideOutVertically { fullHeight -> fullHeight } + shrinkVertically() + fadeOut()
                     ) {
                         BottomNavigationBar(
                             navController,
