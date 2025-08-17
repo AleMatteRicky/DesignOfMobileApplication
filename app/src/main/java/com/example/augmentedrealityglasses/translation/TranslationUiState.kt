b1898aee74f2bcb372b4c601c0ca5595dbaeb7e7
Alessio Ginolfi <alessio.ginolfi@mail.polimi.it>
2025-08-17 23:30:52 +0200

    feat(translation): add currentlyDownloadingLanguageTags in order to show in real time language models downloading

--- app/src/main/java/com/example/augmentedrealityglasses/translation/TranslationUiState.kt
+++ app/src/main/java/com/example/augmentedrealityglasses/translation/TranslationUiState.kt
@@ -1,5 +1,7 @@
 package com.example.augmentedrealityglasses.translation
 
+import kotlinx.coroutines.flow.MutableStateFlow
+
 data class TranslationUiState(
     val sourceLanguage: String? = null,
     val targetLanguage: String? = null,
@@ -12,6 +14,7 @@ data class TranslationUiState(
     val notDownloadedLanguageTags: List<String> = emptyList(),
     val isModelNotAvailable: Boolean = false,
     val currentNormalizedRms: Float = 0f,
+    val currentlyDownloadingLanguageTags: MutableStateFlow<Set<String>> = MutableStateFlow(emptySet()),
     val isResultReady: Boolean = false
 )
 
