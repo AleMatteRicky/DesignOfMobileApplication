84503289c1fe03a2d4fa06af1b4e937610021b32
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2025-08-26 11:30:27 +0200

    feat: add logs when creating new pages

--- esp32/src/view/page/page_factory_impl.cpp
+++ esp32/src/view/page/page_factory_impl.cpp
@@ -5,14 +5,17 @@ std::unique_ptr<Page> PageFactoryImpl::createPage(PageType type) {
     // TODO: add here the remaining cases
     switch (type) {
         case PageType::HOME:
+            Serial.println("Creating the home");
             return Homepage::Factory::create();
         case PageType::WEATHER:
             return WeatherPage::Factory::create();
         case PageType::TRANSLATION:
+            Serial.println("Creating the translation");
             return TranslationPage::Factory::create();
         case PageType::CONNECTION:
-            return Connectionpage::Factory::create();
+            return ConnectionPage::Factory::create();
         default:
+            Serial.println("No suitable page is found, fallback to the home");
             return Homepage::Factory::create();
     }
 }
