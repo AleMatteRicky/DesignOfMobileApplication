63528655bc77926350422e75fca07ca0cea2825c
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2025-08-26 11:23:26 +0200

    feat: set the page in Window as it is passed to the changePage method

--- esp32/src/controller/controller.cpp
+++ esp32/src/controller/controller.cpp
@@ -1,14 +1,18 @@
 #include "controller/controller.h"
 #include "ble/remote_dispatcher.h"
+#include "utility/resource_monitor.h"
 
 namespace controller {
 void CentralController::changePage(view::PageType page) {
-    Serial.println("Chaning page ");
+    Serial.println("Changing page");
+    ResourceMonitor::printRemainingStackSize();
     // TODO: complete the message accordingly to the protocol
     std::string msg = "change page";
-    if (m_model->isConnected())
+    if (m_model->isConnected()) {
+        Serial.println("Model is connected, send the change page message");
         m_model->send(msg);
-    removeFromSubjects();
-    m_window->setPage(m_pageFactory->createPage(view::PageType::HOME));
+    }
+    Serial.println("Setting the current page");
+    m_window->setPage(m_pageFactory->createPage(page));
 }
 }  // namespace controller
