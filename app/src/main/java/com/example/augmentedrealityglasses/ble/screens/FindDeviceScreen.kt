aabeaf3e9b994fe50b7342590f19dfbfb5b6291a
Alessio Ginolfi <alessio.ginolfi@mail.polimi.it>
2025-08-30 16:29:10 +0200

    feat: update FindDeviceScreen ui

--- app/src/main/java/com/example/augmentedrealityglasses/ble/screens/FindDeviceScreen.kt
+++ app/src/main/java/com/example/augmentedrealityglasses/ble/screens/FindDeviceScreen.kt
@@ -4,8 +4,12 @@ import android.annotation.SuppressLint
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.le.ScanFilter
 import android.bluetooth.le.ScanSettings
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,11 +20,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Refresh
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -31,15 +31,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import com.example.augmentedrealityglasses.HomeViewModel
+import com.example.augmentedrealityglasses.Icon
 import com.example.augmentedrealityglasses.ble.characteristic.checkBluetoothConnectPermission
 
 @Composable
@@ -67,9 +71,11 @@ fun FindDeviceScreen(
         scanSettings,
     )
 
-    Column(Modifier
-        .fillMaxSize()
-        .clip(shape = RoundedCornerShape(22.dp))) {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .clip(shape = RoundedCornerShape(22.dp))
+    ) {
         Row(
             Modifier
                 .fillMaxWidth()
@@ -78,27 +84,54 @@ fun FindDeviceScreen(
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically,
         ) {
-            Text(text = "Available devices", style = MaterialTheme.typography.titleSmall)
+            Text(
+                text = "Available Devices",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+
             if (scanning) {
-                CircularProgressIndicator(modifier = Modifier.size(24.dp), strokeWidth = 2.dp)
+                CircularProgressIndicator(
+                    modifier = Modifier.size(27.dp),
+                    strokeWidth = 3.dp,
+                    color = Color.Black,
+                    trackColor = MaterialTheme.colorScheme.surfaceVariant
+                )
             } else {
-                IconButton(
-                    onClick = {
-                        viewModel.scan(filters = null, settings = scanSettings)
-                    },
+                Box(
+                    modifier = Modifier
+                        .size(27.dp)
+                        .clickable(
+                            interactionSource = remember { MutableInteractionSource() },
+                            indication = null,
+                            onClick = {
+                                viewModel.scan(filters = null, settings = scanSettings)
+                            }
+                        ), contentAlignment = Alignment.Center
                 ) {
-                    Icon(imageVector = Icons.Rounded.Refresh, contentDescription = null)
+                    Image(
+                        painter = painterResource(id = Icon.REFRESH.getID()),
+                        contentDescription = "refresh scanning for new devices",
+                        modifier = Modifier.size(27.dp)
+                    )
                 }
             }
         }
 
         LazyColumn(
-            modifier = Modifier.padding(16.dp),
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
             verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             if (devicesOfferingTheService.isEmpty()) {
                 item {
-                    Text(text = "No devices found")
+                    Text(
+                        text = "No devices found",
+                        fontSize = 16.sp
+                    )
+
                 }
             }
 
@@ -136,11 +169,11 @@ internal fun BluetoothDeviceItem(
     ) {
         Text(
             bluetoothDevice.name ?: "N/A",
-            style = if (isSampleServer) {
+            style = if (isSampleServer) { //todo check what is sample server
                 TextStyle(fontWeight = FontWeight.Bold)
             } else {
                 TextStyle(fontWeight = FontWeight.Normal)
-            },
+            }, fontSize = 16.sp
         )
         //Text(bluetoothDevice.address)
 
