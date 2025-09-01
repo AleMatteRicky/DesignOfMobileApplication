2f3d0e44a8167e16af92c725927c8ee427bdd2c0
Alessio Ginolfi <alessio.ginolfi@mail.polimi.it>
2025-09-01 17:52:24 +0200

    feat: update ErrorWrapper ui

--- app/src/main/java/com/example/augmentedrealityglasses/ErrorWrapper.kt
+++ app/src/main/java/com/example/augmentedrealityglasses/ErrorWrapper.kt
@@ -1,8 +1,11 @@
 package com.example.augmentedrealityglasses
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,21 +15,23 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 
 @Composable
 fun ErrorWrapper(
     message: String,
-    durationMillis: Long = 3000, //TODO: make this constant
     onDismiss: () -> Unit,
     content: @Composable () -> Unit
 ) {
+    val durationMillis: Long = 2500
+
     Box(
         modifier = Modifier
             .fillMaxSize(),
 
-    ) {
+        ) {
         content()
         if (message.isNotEmpty()) {
             LaunchedEffect(message) {
@@ -34,20 +39,33 @@ fun ErrorWrapper(
                 onDismiss()
             }
             Box(
-                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp).fillMaxSize(),
+                modifier = Modifier
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+                    .fillMaxSize(),
                 contentAlignment = Alignment.BottomCenter
             ) {
                 Surface(
-                    color = Color.Gray,
+                    color = Color.Black,
                     shape = RoundedCornerShape(8.dp),
                     shadowElevation = 4.dp
                 ) {
-                    Text(
-                        text = message,
-                        color = Color.White,
-                        style = MaterialTheme.typography.bodyMedium,
-                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 10.dp)
-                    )
+                    Row(
+                        Modifier.padding(start = 16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(
+                            painter = painterResource(id = Icon.ERROR.getID()),
+                            contentDescription = "Recording status icon",
+                            Modifier.size(16.dp)
+                        )
+
+                        Text(
+                            text = message,
+                            color = Color.White,
+                            style = MaterialTheme.typography.bodyMedium,
+                            modifier = Modifier.padding(horizontal = 16.dp, vertical = 10.dp)
+                        )
+                    }
                 }
             }
         }
