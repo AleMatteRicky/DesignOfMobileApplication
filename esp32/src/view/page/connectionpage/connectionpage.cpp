a4e59a0e2e5293ff36db2fa40e90c27771f8f2f6
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2025-09-08 11:57:08 +0200

    feat: use text area instead of text

--- esp32/src/view/page/connectionpage/connectionpage.cpp
+++ esp32/src/view/page/connectionpage/connectionpage.cpp
@@ -1,7 +1,8 @@
 #include "view/page/connectionpage/connectionpage.h"
 #include "ble/remote_dispatcher.h"
-#include "controller/controller.h"
+#include "controller/central_controller.h"
 #include "utility/resource_monitor.h"
+#include "view/text/text_area.h"
 #include "view/tft.h"
 
 namespace view {
@@ -9,11 +10,10 @@ namespace view {
 ConnectionPage::ConnectionPage()
     : Page::Page(RectType{Coordinates{0, 0}, Size{SCREEN_WIDTH, SCREEN_HEIGHT}},
                  nullptr),
-      // TODO: dimension and position
-      m_text(new Text(RectType{Coordinates{10, 50}, Size{240, 120}},
-                      nullptr,
-                      "Click for advertising")) {
-    appendSubView(std::unique_ptr<Text>(m_text));
+      m_text(new TextArea(RectType{Coordinates{0, 50}, Size{SCREEN_WIDTH, 120}},
+                          this,
+                          "Click for advertising")) {
+    m_text->setCenter(true, getFrame());
 }
 
 void ConnectionPage::onEvent(ble::ConnectionState const& event) {
