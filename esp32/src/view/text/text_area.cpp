3f29c93086ef9bc50f65533081ade3275463dcf1
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2025-09-08 11:46:35 +0200

    feat: print the text centered with the correct textwidth

--- esp32/src/view/text/text_area.cpp
+++ esp32/src/view/text/text_area.cpp
@@ -1,6 +1,6 @@
 #include "view/text/text_area.h"
 
-#include "fonts/NotoSansMono15pt.h"
+#include "fonts/NotoMono18pt.h"
 #include "utility/resource_monitor.h"
 #include "view/screen/screen.h"
 
@@ -14,57 +14,62 @@ TextArea::TextArea(RectType frame,
       m_fgColour{TFT_LIGHTGREY},
       m_bgColour{TFT_BLACK},
       m_cursorCoordinates{Coordinates{getCoordinates()}},
-      m_wrap{true} {
+      m_oldCursorCoordinates{Coordinates::none},
+      m_wrap{true},
+      m_center{false} {
     auto frameSz = getSize();
     m_tft->setTextDatum(TL_DATUM);
     m_tft->setTextColor(m_fgColour, m_bgColour);
 
+#if UNICODE
     ESP_LOGD(TAG, "State of the memory before loading the font:");
     ResourceMonitor::printRemainingHeapSizeInfo();
-    // m_tft->loadFont(NotoSansMono_15pt);
-    m_tft->setTextFont(1);
-    m_tft->setTextSize(2);
+    if (!isFontAlreadyLoaded)
+        m_tft->loadFont(NotoMono_18pt);
+    isFontAlreadyLoaded = true;
+#else
+    m_tft->setTextFont(2);
+    m_tft->setTextSize(1);
+#endif
+
     m_tft->setTextWrap(false, false);
 
+#if UNICODE
     ESP_LOGD(TAG, "State of the memory after loading the font:");
     ResourceMonitor::printRemainingHeapSizeInfo();
 
+    ESP_LOGD(TAG, "State of the memory before reserving space in advance");
+    ResourceMonitor::printRemainingHeapSizeInfo();
+#endif
+
     m_charHeight = m_tft->fontHeight();
     ESP_LOGD(TAG, "char height: %u", m_charHeight);
-    auto minCharWidth = m_tft->textWidth("a");
-    auto numCharsWorstCase =
-        (frameSz.m_width * frameSz.m_height) / (minCharWidth * m_charHeight);
-    auto numRowsWorstCase = frameSz.m_height / m_charHeight;
-    ESP_LOGD(TAG, "max num of chars per frame: %u\n", numCharsWorstCase);
 
-    // reserve space before hand to avoid dynamic reallocations as new
-    // characters are added to the text
-    // reserve space for new lines to know when break the line as the frame's
-    // borders may not correspond to the screen one.
-    ESP_LOGD(TAG, "Pre allocating %u characters",
-             numCharsWorstCase + numRowsWorstCase);
-    m_currentFrame.allocate(numCharsWorstCase + numRowsWorstCase);
-    m_oldFrame.allocate(numCharsWorstCase + numRowsWorstCase);
+    auto numCharsWorstCase = getNumCharsWorstCase();
+
+    ESP_LOGD(TAG, "max num of chars per frame in the worst case: %u\n",
+             numCharsWorstCase);
+
+    m_currentFrame.allocate(numCharsWorstCase);
+    m_oldFrame.allocate(numCharsWorstCase);
+
+    prepareLineBuffer();
 
     setContent(content);
+
     ESP_LOGD(TAG, "Text created");
 }
 
-size_t TextArea::setContent(std::string const& content) {
-    ESP_LOGD(TAG, "Before setting the content:");
-    ResourceMonitor::printRemainingHeapSizeInfo();
+TextArea::~TextArea() {}
 
+size_t TextArea::setContent(std::string const& content) {
     m_cursorCoordinates = getCoordinates();
 
     m_currentFrame.reset();
 
-    ESP_LOGD(TAG, "After cleaning the current content:");
-    ResourceMonitor::printRemainingHeapSizeInfo();
+    m_curLine[0] = '\0';
 
     size_t charactersWritten = appendContent(content);
-    ESP_LOGD(TAG, "After setting the content:");
-    ResourceMonitor::printRemainingHeapSizeInfo();
-
     return charactersWritten;
 }
 
@@ -86,9 +91,24 @@ size_t TextArea::appendContent(std::string const& content) {
 
     ESP_LOGD(TAG, "Appending content '%s'\n", content.c_str());
 
+    auto numBytesWorstCaseForALine = getNumBytesForALineWorstCase();
+    if (numBytesWorstCaseForALine == 0) {
+        ESP_LOGD(TAG,
+                 "The frame is too small: width = %d, height = %d. No text can "
+                 "be inserted",
+                 frameSz.m_width, frameSz.m_height);
+        return 0;
+    }
+
+    // use a line because counting each character individually may result in
+    // error as the computation would not take into account space adjustements
+    char line[numBytesWorstCaseForALine * 4 + 1];
+
+    // when appending text, the previous line needs to be restored
+    restoreLine(line);
+
     auto cursorCoordinates = m_cursorCoordinates;
     byte inc = 0;
-
     for (uint32_t i = 0; i < content.size(); i += inc) {
         std::string glyph = getUTF8Sequence(content, i);
         assert(glyph.size() > 0 &&
@@ -96,11 +116,14 @@ size_t TextArea::appendContent(std::string const& content) {
 
         inc = glyph.size();
 
-        if (overflowOnX(glyph, cursorCoordinates)) {
+        if (overflowOnX(glyph, line)) {
             ESP_LOGD(TAG,
-                     "Overflowing along the x axis for cursor's coordinates: "
+                     "Overflowing along the x axis for the glyph '%s' and the "
+                     "cursor's coordinates: "
                      "(%d,%d)",
-                     cursorCoordinates.m_x, cursorCoordinates.m_y);
+                     glyph.c_str(), cursorCoordinates.m_x,
+                     cursorCoordinates.m_y);
+
             if (glyph == " " || glyph == "\n")
                 continue;
 
@@ -109,6 +132,7 @@ size_t TextArea::appendContent(std::string const& content) {
 
             cursorCoordinates.m_x = frameCoordinates.m_x;
             cursorCoordinates.m_y += m_tft->fontHeight();
+            line[0] = '\0';
         }
 
         if (overFlowOnY(glyph, cursorCoordinates)) {
@@ -139,26 +163,51 @@ size_t TextArea::appendContent(std::string const& content) {
         if (glyph == "\n") {
             cursorCoordinates = Coordinates{
                 frameCoordinates.m_x, cursorCoordinates.m_y + m_charHeight};
+            line[0] = '\0';
         } else {
             ESP_LOGD(TAG, "'%s' of width %u would be printed at (%u,%u)",
                      glyph.c_str(), m_tft->textWidth(glyph.c_str()),
                      cursorCoordinates.m_x, cursorCoordinates.m_y);
-            cursorCoordinates.m_x += m_tft->textWidth(glyph.c_str()) + 1;
+            strcat(line, glyph.c_str());
+            cursorCoordinates.m_x =
+                frameCoordinates.m_x + m_tft->textWidth(line);
         }
     }
 
     m_cursorCoordinates = cursorCoordinates;
+    strcpy(m_curLine.get(), line);
     return content.size();
 }
 
 void TextArea::drawOnScreen() {
-    m_currentFrame.print();
+    ESP_LOGD(TAG, "Is centered? %d", m_center);
+    ESP_LOGD(TAG, "Cursor at (%d, %d)", m_cursorCoordinates.m_x,
+             m_cursorCoordinates.m_y);
+    ESP_LOGD(TAG, "Old cursor at (%d, %d)", m_oldCursorCoordinates.m_x,
+             m_oldCursorCoordinates.m_y);
 
-    bool invalidated{false};
-    auto frameCoordinates = getCoordinates();
+    // if the old cursor contains data that means the current content will be
+    // drawned at a different position than the old content, hence we need to
+    // clean the screen even if the two contents may have a common prefix
+    if (m_oldCursorCoordinates != Coordinates::none) {
+        auto oldCenter = center(m_oldCursorCoordinates);
+        m_tft->setCursor(oldCenter.m_x, oldCenter.m_y);
+        hide(m_oldFrame, 0);
+    }
+
+    Coordinates cursorCoordinates;
+
+    if (m_center) {
+        cursorCoordinates = center(m_cursorCoordinates);
+    } else {
+        cursorCoordinates = getCoordinates();
+    }
+
+    ESP_LOGD(TAG, "cursor coordinates: (%d, %d)", cursorCoordinates.m_x,
+             cursorCoordinates.m_y);
     auto frameSz = getSize();
-    m_tft->setCursor(frameCoordinates.m_x, frameCoordinates.m_y);
-
+    m_tft->setCursor(cursorCoordinates.m_x, cursorCoordinates.m_y);
+    bool invalidated{false};
     for (uint32_t i = 0; i < m_currentFrame.size(); i++) {
         char const* glyph = m_currentFrame.getGlyphAt(i);
 
@@ -166,8 +215,9 @@ void TextArea::drawOnScreen() {
             if (!invalidated) {
                 int16_t x = m_tft->getCursorX();
                 int16_t y = m_tft->getCursorY();
+                // not called if center==true because the oldFrame is hidden at
+                // the beginning and its size reset
                 hide(m_oldFrame, i);
-                m_oldFrame.eraseFrom(i);
 
                 invalidated = true;
                 // restore the cursor's coordinates changed when hiding the old
@@ -188,13 +238,18 @@ void TextArea::drawOnScreen() {
             // the library automatically sets the cursor on a new line but at
             // the first column of the screen, which may not be what we want:
             // move it at the first column of the frame
-            m_tft->setCursor(frameCoordinates.m_x, m_tft->getCursorY());
+            m_tft->setCursor(cursorCoordinates.m_x, m_tft->getCursorY());
         }
     }
 
     auto curFrameSz = m_currentFrame.size();
     if (m_oldFrame.size() > curFrameSz)
         hide(m_oldFrame, curFrameSz);
+    if (m_center)
+        m_oldCursorCoordinates = m_cursorCoordinates;
+    else
+        // needed when the text is switched from centered to not centered
+        m_oldCursorCoordinates = Coordinates::none;
 }
 
 void TextArea::hideGlyph(char const* glyph) {
@@ -218,6 +273,7 @@ void TextArea::hide(Frame const& textFrame, uint32_t beg) {
             m_tft->setCursor(frameCoordinates.m_x, m_tft->getCursorY());
         }
     }
+    m_oldFrame.eraseFrom(beg);
 }
 
 std::pair<size_t, byte> TextArea::sizeContent(std::string const& content) {
@@ -250,12 +306,92 @@ byte TextArea::getUTF8SequenceLength(unsigned char firstByte) {
     return 0;      // Invalid
 }
 
+void TextArea::setCenter(bool center, RectType const& reference) {
+    m_center = center;
+
+    if (!m_center)
+        return;
+
+    auto sz = getSize();
+    ESP_LOGD(TAG, "reference dim: w = %u, h = %u; current dim: w = %u, h = %u",
+             reference.m_size.m_width, reference.m_size.m_height, sz.m_width,
+             sz.m_height);
+
+    assert(reference.m_size.m_width >= sz.m_width &&
+           reference.m_size.m_height >= sz.m_height &&
+           "The reference must be bigger than this text area");
+
+    m_reference = reference;
+}
+
+bool TextArea::resize(Size const& newSize) {
+    View::resize(newSize);
+    prepareLineBuffer();
+    return true;
+}
+
 void TextArea::clearFromScreen() {
     if (m_currentFrame.size() == 0)
         return;
-    auto [x, y] = getCoordinates();
-    m_tft->setCursor(x, y);
-    hide(m_currentFrame, 0);
+
+    Coordinates cursor = m_oldCursorCoordinates == Coordinates::none
+                             ? getCoordinates()
+                             : m_oldCursorCoordinates;
+
+    ESP_LOGD(TAG, "Clearing from (%d, %d)", cursor.m_x, cursor.m_y);
+
+    m_tft->setCursor(cursor.m_x, cursor.m_y);
+
+    // before: hide(m_currentFrame, 0);
+    hide(m_oldFrame, 0);
 }
 
+Coordinates TextArea::center(Coordinates cursor) {
+    auto [x, y] = m_reference.m_coordinates;
+    auto [w, h] = m_reference.m_size;
+    ESP_LOGD(TAG, "info: pos=(%u, %u), w = %u, h = %u, cursor=(%d,%d)", x, y, w,
+             h, cursor.m_x, cursor.m_y);
+
+    int16_t textWidth;
+    auto frameCoordinates = getCoordinates();
+    auto frameSz = getSize();
+    if (cursor.m_y > frameCoordinates.m_y)
+        textWidth = frameSz.m_width;
+    else
+        textWidth = cursor.m_x - frameCoordinates.m_x;
+
+    ESP_LOGD(TAG, "Computed text width: %d vs actual width: %d", textWidth,
+             m_tft->textWidth(m_curLine.get()));
+
+    int16_t textHeight = cursor.m_y + m_charHeight - frameCoordinates.m_y;
+    ESP_LOGD(TAG, "Text height: %d", textHeight);
+
+    uint16_t deltaX = (std::max(w - textWidth, 0)) / 2;
+    uint16_t deltaY = (std::max(h - textHeight, 0)) / 2;
+
+    Coordinates centeredCoordinates = {x + deltaX, y + deltaY};
+    ESP_LOGD(TAG, "Centered coordinates: (%d,%d)", centeredCoordinates.m_x,
+             centeredCoordinates.m_y);
+    return centeredCoordinates;
+}
+
+void TextArea::restoreLine(char* line) {
+    line[0] = '\0';
+    ESP_LOGD(TAG, "Current line: '%s'", m_curLine.get());
+    strcpy(line, m_curLine.get());
+}
+
+// reserve space before hand to avoid dynamic reallocations as new
+// characters are added to the text
+// reserve space for new lines to know when break the line as the frame's
+// borders may not correspond to the screen one.
+// NOTE. This function depends on the current frame's size and thus needs to be
+// called every time the text area is resized
+void TextArea::prepareLineBuffer() {
+    auto numCharsWorstCase = getNumCharsWorstCase();
+    ESP_LOGD(TAG, "Pre allocating %u characters", numCharsWorstCase);
+
+    m_curLine = std::make_unique<char[]>(getNumBytesForALineWorstCase() + 1);
+    m_curLine[0] = '\0';
+}
 }  // namespace view
