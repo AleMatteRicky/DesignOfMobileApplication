fd870fc87d3ed93813c94e6fdfbd451ba5a83bb7
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2025-09-08 11:14:22 +0200

    feat: add comparison operator for Coordinates

--- esp32/include/view/coordinates.h
+++ esp32/include/view/coordinates.h
@@ -4,7 +4,16 @@
 
 namespace view {
 struct Coordinates {
-    int16_t m_x;
-    int16_t m_y;
+    int m_x;
+    int m_y;
+    static Coordinates none;
+
+    bool operator==(Coordinates const& other) {
+        return m_x == other.m_x && m_y == other.m_y;
+    }
+
+    bool operator!=(Coordinates const& other) {
+        return !(this->operator==(other));
+    }
 };
-}
+}  // namespace view
