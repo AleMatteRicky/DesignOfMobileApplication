8b207e4e4ead8f58bc785e8f653262eb28a3ee21
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2025-09-08 11:16:42 +0200

    feat: add class to monitor the heap usage

--- esp32/include/utility/resource_monitor.h
+++ esp32/include/utility/resource_monitor.h
@@ -3,4 +3,8 @@
 class ResourceMonitor {
 public:
     static void printRemainingStackSize();
+    static void printRemainingHeapSizeInfo();
+
+private:
+    inline static char const TAG[] = "ResourceMonitor";
 };
