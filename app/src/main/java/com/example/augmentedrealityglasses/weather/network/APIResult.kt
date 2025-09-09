b638ed1e40067059602d33e266c23ec53595a1b6
Matteo Gatti <matteo7.gatti@mail.polimi.it>
2025-09-09 14:35:56 +0200

    chore: remove todos and clean up files

--- app/src/main/java/com/example/augmentedrealityglasses/weather/network/APIResult.kt
+++ app/src/main/java/com/example/augmentedrealityglasses/weather/network/APIResult.kt
@@ -3,7 +3,6 @@ package com.example.augmentedrealityglasses.weather.network
 sealed class APIResult<out T> {
     data class Success<out T>(val value: T) : APIResult<T>()
 
-    //TODO: make error a data class
     data class GenericError(val code: Int? = null, val error: String) : APIResult<Nothing>()
     object NetworkError : APIResult<Nothing>()
 }
